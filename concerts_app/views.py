9085019f2f50448042f9a3e2cc4fb0561e591c6c
Sergio <sergioguijarroc@gmail.com>
2024-01-25 12:51:12 +0100

    Reestructurados los templates de Conciertos en una subcarpeta llamada conciertos

--- concerts_app/views.py
+++ concerts_app/views.py
@@ -7,7 +7,7 @@ from django.views.generic import (
     UpdateView,
     DeleteView,
 )
-from .models import Concierto
+from .models import Concierto, Artista
 
 # Create your views here.
 
@@ -16,15 +16,18 @@ def index(request):
     return render(request, "concerts_app/panelControl.html")
 
 
+# region Conciertos
+
+
 # Usuarios normales
-
-
 class ConciertoListView(ListView):
     model = Concierto
+    template_name = "concerts_app/conciertos/concierto_list.html"
 
 
 class ConciertoDetailView(DetailView):
     model = Concierto
+    template_name = "concerts_app/conciertos/concierto_create.html"
 
 
 # Staff
@@ -32,16 +35,53 @@ class ConciertoCreateView(CreateView):
     model = Concierto
     fields = "__all__"
     success_url = reverse_lazy("concierto_list")
-    template_name = "concerts_app/concierto_create.html"
+    template_name = "concerts_app/conciertos/concierto_create.html"
 
 
 class ConciertoDeleteView(DeleteView):
     model = Concierto
     success_url = reverse_lazy("concierto_list")
+    template_name = "concerts_app/conciertos/concierto_confirm_delete.html"
 
 
 class ConciertoUpdateView(UpdateView):
     model = Concierto
     fields = "__all__"
     success_url = reverse_lazy("concierto_list")
-    template_name = "concerts_app/concierto_update.html"
+    template_name = "concerts_app/conciertos/concierto_update.html"
+
+
+# endregion
+# region Artistas
+
+
+# Usuarios normales
+class ArtistaListView(ListView):
+    model = Artista
+
+
+class ArtistaDetailView(DetailView):
+    model = Artista
+
+
+# Staff
+class ArtistaCreateView(CreateView):
+    model = Artista
+    fields = "__all__"
+    success_url = reverse_lazy("concierto_list")
+    template_name = "concerts_app/artista_create.html"
+
+
+class ArtistaDeleteView(DeleteView):
+    model = Artista
+    success_url = reverse_lazy("artista_list")
+
+
+class ArtistaUpdateView(UpdateView):
+    model = Artista
+    fields = "__all__"
+    success_url = reverse_lazy("concierto_list")
+    template_name = "concerts_app/artista_update.html"
+
+
+# endregion
